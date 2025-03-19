2b7e1a6d6c73b64bff21482493b459301369226a
cybergausho <lauttmz@gmail.com>
2025-03-18 21:34:42 -0300

    GIT EXPLAIN

--- hello_git/Program.cs
+++ hello_git/Program.cs
@@ -10,6 +10,19 @@ namespace hello_git
         }
 
     }
+    /*
+     PROCEDIMIENTO GIT:
+    (repositorio LOCAL)
+    1- git init. (solo cuando se inicializa)
+    -- bucle de trabajo --
+    2- git add
+    3- git commit -m "mensaje referencial a lo que voy subir :)"
+    (repostorio REMOTO)
+    4- git push (ENVIAR LOS CAMBIOS)
+    -----------------------------------
+    5- git pull (TRAERME LOS CAMBIOS)
+         
+     */
 
     public class pokemon
     {
